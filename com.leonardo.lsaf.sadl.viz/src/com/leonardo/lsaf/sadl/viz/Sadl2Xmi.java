503d2dfaa580b4beb127ca426aaf406dddb50e39
Alfa Yohannis <alfa.yohannis@york.ac.uk>
2023-09-07 08:47:37 +0100

    Update Template and sample

--- com.leonardo.lsaf.sadl.viz/src/com/leonardo/lsaf/sadl/viz/Sadl2Xmi.java
+++ com.leonardo.lsaf.sadl.viz/src/com/leonardo/lsaf/sadl/viz/Sadl2Xmi.java
@@ -1,12 +1,8 @@
 package com.leonardo.lsaf.sadl.viz;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.XMIResource;
@@ -16,9 +12,6 @@ import org.eclipse.xtext.resource.XtextResourceSet;
 
 import com.google.inject.Injector;
 import com.leonardo.lsaf.sadl.SADLStandaloneSetup;
-import com.leonardo.lsaf.sadl.sadl.Connection;
-import com.leonardo.lsaf.sadl.sadl.Model;
-import com.leonardo.lsaf.sadl.sadl.Package;
 
 public class Sadl2Xmi {
 
@@ -34,26 +27,26 @@ public class Sadl2Xmi {
 			XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
 			resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 			Resource resource = resourceSet.getResource(URI.createFileURI(sourceModelFile.getAbsolutePath()), true);
-			
-			TreeIterator<EObject> iterator = resource.getAllContents();
-			while(iterator.hasNext()) {
-				EObject eObject = iterator.next();
-				System.out.println(eObject);
-				if (eObject instanceof Connection) {
-					Connection con = (Connection) eObject;
-					System.out.println(con.getDestination().get(con.getDestination().size() - 1).getName());
-					System.out.println(con.getSource().get(con.getSource().size() - 1).getName());
-					System.out.println(con.getFrom().getName());
-					System.out.println(con.getTo().getName());
-					System.console();
-				}
-			}
+//			
+//			TreeIterator<EObject> iterator = resource.getAllContents();
+//			while(iterator.hasNext()) {
+//				EObject eObject = iterator.next();
+//				System.out.println(eObject);
+//				if (eObject instanceof Connection) {
+//					Connection con = (Connection) eObject;
+//					System.out.println(con.getDestination().get(con.getDestination().size() - 1).getName());
+//					System.out.println(con.getSource().get(con.getSource().size() - 1).getName());
+//					System.out.println(con.getFrom().getName());
+//					System.out.println(con.getTo().getName());
+//					System.console();
+//				}
+//			}
 
 			XMIResource xmiResource = new XMIResourceImpl(
 					URI.createFileURI(sourceModelFile.getAbsolutePath() + ".xmi"));
 			xmiResource.getContents().addAll(EcoreUtil.copyAll(resource.getContents()));
 
-//			xmiResource.save(null);
+			xmiResource.save(null);
 			System.out.println("Done");
 		} catch (Exception e) {
 			e.printStackTrace();
